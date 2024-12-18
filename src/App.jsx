81f960b128a4836ad56c7419c712bb15a1cb9cad
DanielGuillen88 <danielguillen85@gmail.com>
2024-12-18 11:56:04 +0100

    add router in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import HolaMundo from './components/HolaMundo'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import EventPage from "./pages/EventPage";
+import CommunityPage from "./pages/CommunityPage";
+import LoginPage from "./pages/LoginPage";
+import { UserProvider } from "./context/userContext";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0) // ref de Vue
 
   return (
-    <div>
-        <HolaMundo/> 
-    </div>
+    <UserProvider>
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+
+    <Route index element={<HomePage />} />
+    <Route path="EventPage" element={<EventPage />} />
+    <Route path="CommunityPage" element={<CommunityPage />} />
+    <Route path="LoginPage" element={<LoginPage />} />
+
+    </Routes>
+    </BrowserRouter>
+    </UserProvider>
   )
 }
 
