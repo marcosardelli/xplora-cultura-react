8d4fe0064fd9bccff4dccf7f1964b45ce3e03cfb
DanielGuillen88 <danielguillen85@gmail.com>
2024-12-19 13:31:26 +0100

    import css on app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import { UserProvider } from "./context/userContext";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import './App.css'
 
 function App() {
 
